6d308e65b9b5575408812e57e3378929dd9fbb55
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2023-06-29 17:40:02 -0300

    Initial Commit

---
+++ AprendendoGitEGitHub/AprendendoGitEGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  AprendendoGitEGitHubApp.swift
+//  AprendendoGitEGitHub
+//
+//  Created by Gustavo Horestee Santos Barros on 29/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AprendendoGitEGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AprendendoGitEGitHub/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AprendendoGitEGitHub
+//
+//  Created by Gustavo Horestee Santos Barros on 29/06/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
